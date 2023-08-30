6d2b48db746aa5a8a3f02731dc5a122b32c2d97e
AlexandreFelippe <alemfelippe@gmail.com>
2023-08-30 14:48:53 -0300

    Commit inicial

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,7 +1,22 @@
 // import user from './user';
 // import wallet from './wallet';
 
+import { combineReducers } from 'redux';
+
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
 
-export default () => {}; // delete essa linha e configure os seus reducers
+const INITIAL_STATE = {};
+
+const exampleReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+const rootReducer = combineReducers({ exampleReducer });
+
+export default rootReducer;
+
+// export default () => {}; // delete essa linha e configure os seus reducers
